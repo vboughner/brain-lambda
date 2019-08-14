fdab0a5f53db1fd8d1f286b82fc78098b1c4467e
Van Boughner <van@viv.ai>
2019-08-14 09:55:12 -0700

    updated code to make security access levels cleaner

--- src/index.js
+++ src/index.js
@@ -9,7 +9,11 @@ const timeModule = require('./time')
 const error = require('./error.js')
 
 const SECRET_CLIENT_API_KEY = process.env['SECRET_CLIENT_API_KEY']
-const REPORT_GENERATION_API_KEY = process.env['REPORT_GENERATION_API_KEY'] // may only generate reports
+const REPORT_GENERATION_API_KEY = process.env['REPORT_GENERATION_API_KEY']
+const ACCESS_LEVEL_SECRETS = 'all-secrets'
+const ACCESS_LEVEL_REPORTS = 'reports-only'
+const ACCESS_LEVEL_NONE = 'none'
+
 const CLIENT_VERSION_SEMVER_SATISFIES = '1.x'
 const SERVER_VERSION = '1.1.1'
 
@@ -21,72 +25,104 @@ exports.handler = async (event) => {
     if (!body) {
         return error.getResponse(error.MISSING_BODY)
     }
-    const request = {
-        ...body,
-        secretClientApiKey: 'redacted',
-    }
-    console.log('request:', JSON.stringify(request, null, 2))
+    logRequest(body)
 
     const clientVersion = body['clientVersion']
-    let onlyReportGenerationAllowed = false
     if (!clientVersion || !semver.satisfies(clientVersion, CLIENT_VERSION_SEMVER_SATISFIES)) {
         return error.getResponse(error.INCORRECT_CLIENT_VERSION)
     }
-    if (REPORT_GENERATION_API_KEY && REPORT_GENERATION_API_KEY === body['reportGenerationApiKey']) {
-        onlyReportGenerationAllowed = true
-    } else if (!SECRET_CLIENT_API_KEY || SECRET_CLIENT_API_KEY !== body['secretClientApiKey']) {
+
+    const accessLevel = determineAccessLevel(body)
+    console.log('access level is', accessLevel)
+    if (accessLevel === ACCESS_LEVEL_NONE) {
         return error.getResponse(error.INCORRECT_CLIENT_AUTH)
     }
+
     const userId = body['userId']
     if (!userId) {
         return error.getResponse(error.MISSING_USER_ID)
     }
     const myBrainUserId = await dbModule.getMyBrainUserId(userId);
     const deviceId = body['deviceId'] || body['deviceModel'] || 'unknown-device-id'
-    const actionType = body['actionType'] || 'unknown-action-type'
-    console.log('myBrainUserId is', myBrainUserId, ', deviceId is', deviceId, ', and actionType is', actionType)
+    console.log('myBrainUserId is', myBrainUserId, ', deviceId is', deviceId)
 
+    if (accessLevel === ACCESS_LEVEL_SECRETS) {
+        return await allSecretsAccessHandler(myBrainUserId, deviceId, body)
+    }
+    if (accessLevel === ACCESS_LEVEL_REPORTS) {
+        return await reportsOnlyAccessHandler(myBrainUserId, deviceId, body)
+    }
+    return error.getResponse(error.UNSPECIFIED, 'unknown access level')
+}
+
+const logRequest = (request) => {
+    const redactedRequest = {
+        ...request,
+        reportGenerationApiKey: request.reportGenerationApiKey ? 'redacted' : undefined,
+        secretClientApiKey: request.secretClientApiKey ? 'redacted' : undefined,
+    }
+    console.log('redacted request:', JSON.stringify(redactedRequest, null, 2))
+}
+
+const determineAccessLevel = (request) => {
+    if (SECRET_CLIENT_API_KEY && SECRET_CLIENT_API_KEY === request['secretClientApiKey']) {
+        return ACCESS_LEVEL_SECRETS
+    }
+    if (REPORT_GENERATION_API_KEY && REPORT_GENERATION_API_KEY === request['reportGenerationApiKey']) {
+        return ACCESS_LEVEL_REPORTS
+    }
+    return ACCESS_LEVEL_NONE
+}
+
+const allSecretsAccessHandler = async (userId, deviceId, request) => {
     let response;
-    if (actionType === ACTION_GET_REPORT) {
+    const actionType = request['actionType'] || 'unknown-action-type'
+    if (request['statement']) {
+        let cleanText = wordModule.cleanUpResponseText(request['statement'])
+        const completeResponse = await getResponseToStatement(userId, deviceId, cleanText)
+        response = wrap(completeResponse)
+    } else if (request['question']) {
+        let cleanText = wordModule.cleanUpResponseText(request['question'])
+        const completeResponse = await getResponseToQuestion(userId, deviceId, cleanText)
+        response = completeResponse ? wrap(completeResponse) : error.getResponse(error.EMPTY_QUESTION)
+    } else if (request['deleteAll']) {
+        const completeResponse = await deleteAll(userId, deviceId)
+        response = completeResponse ? wrap(completeResponse) : error.getResponse(error.DELETE_ALL_FAILED)
+    } else if (request['deleteOne']) {
+        const whenStored = request['whenStored']
+        if (whenStored) {
+            const completeResponse = await deleteOne(userId, deviceId, whenStored)
+            response = completeResponse ? wrap(completeResponse) : error.getResponse(error.DELETE_ONE_FAILED)
+        } else {
+            return error.getResponse(error.MISSING_WHEN_STORED)
+        }
+    } else if (request['list']) {
+        const completeResponse = await getList(userId, deviceId)
+        response = completeResponse ? wrap(completeResponse) : error.getResponse(error.UNSPECIFIED, 'problem with list')
+    } else if (actionType === ACTION_GET_REPORT) {
         const completeResponse = await getReport()
         response = completeResponse ? wrap(completeResponse) : error.getResponse(error.UNSPECIFIED, 'problem with report')
-    } else if (onlyReportGenerationAllowed) {
-        return error.getResponse(error.REPORT_API_KEY_EXCEEDED)
+    } else if (actionType === ACTION_GET_REPORT) {
+        const completeResponse = await getReport(userId, deviceId)
+        response = completeResponse ? wrap(completeResponse) : error.getResponse(error.UNSPECIFIED, 'problem with report')
     } else {
-        // any operation requiring full permissions must be in here
-        if (body['statement']) {
-            let cleanText = wordModule.cleanUpResponseText(body['statement'])
-            const completeResponse = await getResponseToStatement(myBrainUserId, deviceId, cleanText)
-            response = wrap(completeResponse)
-        } else if (body['question']) {
-            let cleanText = wordModule.cleanUpResponseText(body['question'])
-            const completeResponse = await getResponseToQuestion(myBrainUserId, deviceId, cleanText)
-            response = completeResponse ? wrap(completeResponse) : error.getResponse(error.EMPTY_QUESTION)
-        } else if (body['deleteAll']) {
-            const completeResponse = await deleteAll(myBrainUserId, deviceId)
-            response = completeResponse ? wrap(completeResponse) : error.getResponse(error.DELETE_ALL_FAILED)
-        } else if (body['deleteOne']) {
-            const whenStored = body['whenStored']
-            if (whenStored) {
-                const completeResponse = await deleteOne(myBrainUserId, deviceId, whenStored)
-                response = completeResponse ? wrap(completeResponse) : error.getResponse(error.DELETE_ONE_FAILED)
-            } else {
-                return error.getResponse(error.MISSING_WHEN_STORED)
-            }
-        } else if (body['list']) {
-            const completeResponse = await getList(myBrainUserId, deviceId)
-            response = completeResponse ? wrap(completeResponse) : error.getResponse(error.UNSPECIFIED, 'problem with list')
-        } else if (actionType === ACTION_GET_REPORT) {
-            const completeResponse = await getReport()
-            response = completeResponse ? wrap(completeResponse) : error.getResponse(error.UNSPECIFIED, 'problem with report')
-        } else {
-            response = error.getResponse(error.MISSING_API_COMMAND)
-        }
+        response = error.getResponse(error.MISSING_API_COMMAND)
     }
 
     console.log('response:', JSON.stringify(response, null, 2))
     return response
-};
+}
+
+const reportsOnlyAccessHandler = async (userId, deviceId, request) => {
+    const actionType = request['actionType'] || 'unknown-action-type'
+    if (actionType === ACTION_GET_REPORT) {
+        const completeResponse = await getReport(userId, deviceId)
+        const response = completeResponse ? wrap(completeResponse) : error.getResponse(error.UNSPECIFIED, 'problem with report')
+        console.log('response:', JSON.stringify(response, null, 2))
+        return response
+    }
+    return error.getResponse(error.REPORT_API_KEY_EXCEEDED)
+}
 
 // returns a response wrapped in a success code, if it was successful
 function wrap(completeResponse) {
